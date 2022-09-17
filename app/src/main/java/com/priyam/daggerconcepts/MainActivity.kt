7b62a6e2a7072f7cfe50695a5b19ece564a6ae9b
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-18 01:01:15 +0530

    Manual Dependency Injection

--- app/src/main/java/com/priyam/daggerconcepts/MainActivity.kt
+++ app/src/main/java/com/priyam/daggerconcepts/MainActivity.kt
@@ -7,8 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val userRepository  = UserRepository()
+        val welcomeEmail = WelcomeEmail()
 
-        val userRegistrationService = UserRegistrationService()
+        val userRegistrationService = UserRegistrationService(userRepository, welcomeEmail)
         userRegistrationService.registerUser("Falooda@gmail.com", "123456")
 
     }
--- app/src/main/java/com/priyam/daggerconcepts/UserRegistrationService.kt
+++ app/src/main/java/com/priyam/daggerconcepts/UserRegistrationService.kt
@@ -1,10 +1,11 @@
 package com.priyam.daggerconcepts
 
-class UserRegistrationService {
-    private val userRepository = UserRepository()
-    private val welcomeEmail = WelcomeEmail()
+class UserRegistrationService(
+    private val userRepository: UserRepository,
+    private val welcomeEmail: WelcomeEmail
+) {
 
-    fun registerUser(email: String , password:String){
+    fun registerUser(email: String, password: String) {
         userRepository.saveUser(email, password)
         welcomeEmail.send(email, "pp@gmail.com", "User Registered")
     }
